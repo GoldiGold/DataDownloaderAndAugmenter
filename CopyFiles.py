3a10a03034b45408e1f95143f854413024e60154
GoldiGold <17chen07@gmail.com>
2022-04-11 03:25:26 +0300

    copied t1w and masks

--- CopyFiles.py
+++ CopyFiles.py
@@ -1,28 +1,53 @@
 import os
 import shutil
-t1w_src_path_suffix = 'enter the real one'
-t1w_dst_path_suffix = 'enter the real one'
+import consts
+
+t1w_src_path_suffix = f'MNINonLinear/{consts.T1W_NAME}'
+t1w_dst_path_suffix = f'{consts.T1W_NAME}'
+mask_src_path_suffix = f'MNINonLinear/{consts.MASK_NAME}'
+mask_dst_path_suffix = f'{consts.MASK_NAME}'
+
 
 def copy_masks(src_dataset: str, dst_dataset: str):
-	indices = [str(i) for i in os.listdir(src_dataset) if i.isdigit()]
-	for idx in indices:
-		t1w_src_file = os.path.join(src_dataset, idx, t1w_src_path_suffix)
-		t1w_path_dst = os.path.join(dst_dataset, 'T1w', idx)
-		if os.path.exists(t1w_src_file) and os.path.isfile(t1w_src_file):
-			if os.path.isdir(t1w_path_dst):
-				t1w_dst_file = os.path.join(t1w_path_dst, t1w_dst_path_suffix)
-				if not os.path.exists(t1w_dst_file):
-					shutil.copy(t1w_src_file, t1w_dst_file)
-		else: 
-			print(f'ERROR couldnt copy t1w of brain number {idx}')
-		mask_src_file = os.path.join(src_dataset, idx, mask_src_path_suffix)
-		mask_path_dst = os.path.join(dst_dataset, 'General-Masks', idx)
-		if os.path.exists(mask_src_file) and os.path.isfile(mask_src_file):
-			if not os.path.exists(mask_path_dst):
-				os.mkdir(mask_path_dst)
-			if os.path.isdir(mask_path_dst):
-				mask_dst_file = os.path.join(mask_path_dst, mask_dst_path_suffix)
-				if not os.path.exists(mask_dst_file):
-					shutil.copy(mask_src_file, mask_dst_file)
-		else:
-			print(f'ERROR couldnt copy mask of brain number {idx}')
+    indices = [str(i) for i in os.listdir(src_dataset) if i.isdigit()]
+    copied_t1w_counter = 0
+    copied_masks_counter = 0
+    failed_t1w_counter = 0
+    failed_masks_counter = 0
+    for idx in indices:
+        t1w_src_file = os.path.join(src_dataset, idx, t1w_src_path_suffix)
+        t1w_path_dst = os.path.join(dst_dataset, 'T1w', idx)
+        if os.path.exists(t1w_src_file) and os.path.isfile(t1w_src_file):
+            if not os.path.exists(t1w_path_dst):
+                os.mkdir(t1w_path_dst)
+            if os.path.isdir(t1w_path_dst):
+                t1w_dst_file = os.path.join(t1w_path_dst, t1w_dst_path_suffix)
+                if not os.path.exists(t1w_dst_file):
+                    shutil.copy(t1w_src_file, t1w_dst_file)
+                    copied_t1w_counter += 1
+        else:
+            print(f'ERROR couldnt copy t1w of brain number {idx}')
+            failed_t1w_counter += 1
+        mask_src_file = os.path.join(src_dataset, idx, mask_src_path_suffix)
+        mask_path_dst = os.path.join(dst_dataset, 'General-Masks', idx)
+        if os.path.exists(mask_src_file) and os.path.isfile(mask_src_file):
+            if not os.path.exists(mask_path_dst):
+                os.mkdir(mask_path_dst)
+            if os.path.isdir(mask_path_dst):
+                mask_dst_file = os.path.join(mask_path_dst, mask_dst_path_suffix)
+                if not os.path.exists(mask_dst_file):
+                    shutil.copy(mask_src_file, mask_dst_file)
+                    copied_masks_counter += 1
+        else:
+            print(f'ERROR couldnt copy mask of brain number {idx}')
+            failed_masks_counter += 1
+        # break  # TO check for the first brain
+    print(
+        f't1w: {copied_t1w_counter}, masks: {copied_masks_counter}, failed t1w: {failed_t1w_counter}, failed masks: {failed_masks_counter}')
+    print(f'amount of brains to copy: {len(indices)}')
+
+
+if __name__ == '__main__':
+    src_dataset = consts.DATASET_DIR
+    dst_dataset = consts.SSD_DATASET
+    copy_masks(src_dataset=src_dataset, dst_dataset=dst_dataset)
--- CreateSerialDataset.py
+++ CreateSerialDataset.py
@@ -1,4 +1,6 @@
 import os
+import consts
+import shutil
 
 serial_dataset_path = 'sdp'
 og_dataset_path = 'odp'
@@ -6,7 +8,7 @@ og_t1w_path = 'otp'
 og_masks_path = 'omp'
 og_new_masks_path = 'omnp'
 
-def create_serial_dataset(og_dataset: str, new_dataset: str):
+def create_serial_dataset(src_dataset: str, dst_dataset: str):
 	indices = [str(i) for i in os.listdir(src_dataset) if i.isdigit()]
 	for sidx, idx in enumerate(indices):
 		t1w_src_file = os.path.join(src_dataset, idx, t1w_src_path_suffix)
@@ -15,7 +17,7 @@ def create_serial_dataset(og_dataset: str, new_dataset: str):
 			if os.path.isdir(t1w_path_dst):
 				t1w_dst_file = os.path.join(t1w_path_dst, t1w_dst_path_suffix)
 				if not os.path.exists(t1w_dst_file):
-					copy(t1w_src_file, t1w_dst_file)
+					shutil.copy(t1w_src_file, t1w_dst_file)
 		else: 
 			print(f'ERROR couldnt copy t1w of brain number {idx}')
 		mask_src_file = os.path.join(src_dataset, idx, mask_src_path_suffix)
@@ -26,7 +28,7 @@ def create_serial_dataset(og_dataset: str, new_dataset: str):
 			if os.path.isdir(mask_path_dst):
 				mask_dst_file = os.path.join(mask_path_dst, mask_dst_path_suffix)
 				if not os.path.exists(mask_dst_file):
-					copy(mask_src_file, mask_dst_file)
+					shutil.copy(mask_src_file, mask_dst_file)
 		else:
 			print(f'ERROR couldnt copy mask of brain number {idx}')
 	
